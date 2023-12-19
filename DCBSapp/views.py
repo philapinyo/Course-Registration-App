95f17816601ce50597d8392bf4f7f551aa8f1320
philapinyo <stormjanes@gmail.com>
2023-12-19 22:19:07 +0000

    Third commit

--- DCBSapp/views.py
+++ DCBSapp/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 def home(request):
 
-    return render(request, 'home.html')
+    return render(request, 'home.html', context={})
+
 
 def about(request):
     
